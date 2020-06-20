d3b096fb80452a5d0016b7589086eae3b193bcb7
Max428 <55417346+Max428@users.noreply.github.com>
2020-06-20 18:49:06 +0200

    Update customer.js
    Add user.lastname

--- src/Components/CustomerName.js
+++ src/Components/CustomerName.js
@@ -23,7 +23,7 @@ class CustomerName extends React.Component {
             <h2 type="text" name="firstname">{user.firstName}</h2>
             </td>
             <td>
-            <h2 type="text" name="lastname"></h2>
+            <h2 type="text" name="lastname">{user.lastName}</h2>
             </td>
             </tr>
             <p>Person Nr/Organisations Nr</p>
